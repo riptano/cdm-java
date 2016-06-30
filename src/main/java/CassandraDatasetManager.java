51d4caf87bc4d21bf0d9406f9c46e0e06edf47e9
Jon Haddad <jon@jonhaddad.com>
2016-06-29 17:29:10 -0700

    fetching remote yaml file

--- src/main/java/CassandraDatasetManager.java
+++ src/main/java/CassandraDatasetManager.java
@@ -1,6 +1,11 @@
 import com.datastax.driver.core.Cluster;
+import org.eclipse.jgit.api.Git;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by jhaddad on 6/29/16.
@@ -8,21 +13,30 @@ import java.io.File;
 
 public class CassandraDatasetManager {
 
-    private static final String YAML_LOCATION = "https://github.com/github/testrepo.git";
+    private static final String YAML_URI = "https://raw.githubusercontent.com/riptano/cdm/master/datasets.yaml";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
 
         // check for the .cdm directory
         String home_dir = System.getProperty("user.home");
         System.out.println(home_dir);
-        File f = new File(home_dir + "/.cdm");
+        String cdm_path = home_dir + "/.cdm";
+
+        File f = new File(cdm_path);
         System.out.println(f);
+
         f.mkdir();
+
+        // check for the YAML file
+        File yaml = new File(cdm_path + "/cdm.yaml");
+        if (!yaml.exists()) {
+            URL y = new URL(YAML_URI);
+            FileUtils.copyURLToFile(y, yaml);
+        }
 
         // parse the CLI options
 
-        // check for the YAML file
 
         // create a cluster and session
 
